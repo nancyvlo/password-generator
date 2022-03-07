22fac44c39d2216d6af8044388c7c7c1c4e9098a
Nancy <nancyle.velasco@gmail.com>
2022-03-06 20:54:35 -0600

    assignment code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 // Assignment code here
+var password=document.getElementById("password");
+var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ var passwordLength = 12;
+ var password = "";
 
+ for (var i = 0; i <= passwordLength; i++) {
+  var randomNumber = Math.floor(Math.random() * chars.length);
+  password += chars.substring(randomNumber, randomNumber +1);
+ }
+ document.getElementById("password").value = password;
+
+ function copyPassword() {
+  var copyText = document.getElementById("password");
+  copyText.select();
+  
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -7,7 +22,7 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   console.log("Create Password");
 // 1. Prompt the user for the password criteria
-//    a. Password length 8 < 128
+//    a. Password length 8 < 12
 //    b. Lowercase, uppercase, numbers, special characters
 // 2. Validate the input.
 // 3. Display password to the page.
@@ -29,3 +44,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
