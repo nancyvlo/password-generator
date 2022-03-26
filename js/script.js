dbb34337b21de8d27e12a34d96c0c02b31f584ff
Nancy <nancyle.velasco@gmail.com>
2022-03-26 17:20:55 -0500

    js update redoing work to make questions pop up

--- js/script.js
+++ js/script.js
@@ -1,59 +1,134 @@
-// Assignment code here
-var password = document.getElementById("password")
 
-function genPassword() {
-  var allUserChars = []
-  var numbersChars = ['0','1','2','3','4','5','6','7','8','9']
-  var uppercaseChars = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-  var lowercaseChars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-  var specialChars = ['!','@','#','$','%','^','&','*']
+var enter;
+var confirmNumber;
+var confirmCharacter;
+var confirmUppercase;
+var confirmLowercase;
 
-    var howManychars = prompt("How long do you want your password to be?")
-    var howManychars = confirm("How long do you want your password to be?")
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 
-    var wantsNumber = prompt("Do you want numbers?")
-    var wantsNumber = confirm("Do you want numbers?")
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-    var wantsCapital = prompt("Do you want uppercase letters?")
-    var wantsCapital = confirm("Do you want uppercase letters?")
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-    var wantsLowercase = prompt("Do you want lowercase letters?")
-    var wantsLowercase = confirm("Do you want lowercase letters?")
+space = [];
 
-    var wantsspecialChars = prompt("Do you want special characters?")
-    var wantsspecialChars = confirm("Do you want special characters?")
+var choices;
 
-    if (wantsNumber){allUserChars = allUserChars.concat(numbersChars)}
-    if (wantsCapital){allUserChars = allUserChars.concat(uppercaseChars)}
-    if (wantsLowercase){allUserChars = allUserChars.concat(lowercaseChars)}
-    if (wantsSpecial){allUserChars = allUserChars.concat(specialChars)}
+var toUpper = function (x) {
+    return x.toUpperCase();
+};
+
+alpha2 = alpha.map(toUpper);
+
+var get = document.querySelector("#generate");
+
+get.addEventListener("click", function () {
+    ps = generatePassword();
+    document.getElementById("password").placeholder = ps;
+});
+
+
+function generatePassword() {
     
-    console.log(allUserChars)
-    var passwordLength = 12;
-    var password = "";
+    enter = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
+    
+    if (!enter) {
+        alert("This needs a value");
+    } else if (enter < 8 || enter > 128) {
+        
+        enter = parseInt(prompt("You must choose between 8 and 128"));
 
-    for (var i = 0; i <= passwordLength; i++) {
-      var randomNumber = Math.floor(Math.random()*Chars.length);
-      password <= chars.substring(randomNumber, random +1)
+    } else {
+        
+        confirmNumber = confirm("Will this contain numbers?");
+        confirmCharacter = confirm("Will this contain special characters?");
+        confirmUppercase = confirm("Will this contain Uppercase letters?");
+        confirmLowercase = confirm("Will this contain Lowercase letters?");
+    };
+
+   
+    if (!confirmCharacter && !confirmNumber && !confirmUppercase && !confirmLowercase) {
+        choices = alert("You must choose a criteria!");
+
     }
-    return "generated password goes here"
-}
-  document.getElementById("password").value = password;
+    else if (confirmCharacter && confirmNumber && confirmUppercase && confirmLowercase) {
+
+        choices = character.concat(number, alpha, alpha2);
+    }
+   
+    else if (confirmCharacter && confirmNumber && confirmUppercase) {
+        choices = character.concat(number, alpha2);
+    }
+    else if (confirmCharacter && confirmNumber && confirmLowercase) {
+        choices = character.concat(number, alpha);
+    }
+    else if (confirmCharacter && confirmLowercase && confirmUppercase) {
+        choices = character.concat(alpha, alpha2);
+    }
+    else if (confirmNumber && confirmLowercase && confirmUppercase) {
+        choices = number.concat(alpha, alpha2);
+    }
     
+    else if (confirmCharacter && confirmNumber) {
+        choices = character.concat(number);
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-  
+    } else if (confirmCharacter && confirmLowercase) {
+        choices = character.concat(alpha);
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    } else if (confirmCharacter && confirmUppercase) {
+        choices = character.concat(alpha2);
+    }
+    else if (confirmLowercase && confirmNumber) {
+        choices = alpha.concat(number);
 
-  passwordText.value = password;
+    } else if (confirmLowercase && confirmUppercase) {
+        choices = alpha.concat(alpha2);
+
+    } else if (confirmNumber && confirmUppercase) {
+        choices = number.concat(alpha2);
+    }
+   
+    else if (confirmCharacter) {
+        choices = character;
+    }
+    else if (confirmNumber) {
+        choices = number;
+    }
+    else if (confirmLowercase) {
+        choices = alpha;
+    }
+    
+    else if (confirmUppercase) {
+        choices = space.concat(alpha2);
+    };
+
+    
+    var password = [];
+
+    
+    for (var i = 0; i < enter; i++) {
+        var pickChoices = choices[Math.floor(Math.random() * choices.length)];
+        password.push(pickChoices);
+    }
+    
+    var ps = password.join("");
+    UserInput(ps);
+    return ps;
+}
+
+function UserInput(ps) {
+    document.getElementById("password").textContent = ps;
 
 }
 
+var copy = document.querySelector("#copy");
+copy.addEventListener("click", function () {
+    copyPassword();
+});
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function copyPassword() {
+    document.getElementById("password").select();
+    document.execCommand("Copy");
+    alert("Password copied to clipboard!");
+}
